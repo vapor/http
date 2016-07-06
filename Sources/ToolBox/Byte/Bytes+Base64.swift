42c06176e6f5733131824aeb3c9f5013bc4d7cb6
Logan Wright <logan.william.wright@gmail.com>
2016-07-06 02:58:31 -0400

    tweak base64

--- Sources/ToolBox/Byte/Bytes+Base64.swift
+++ Sources/ToolBox/Byte/Bytes+Base64.swift
@@ -10,8 +10,8 @@ import Foundation
 
 extension Sequence where Iterator.Element == Byte {
     public var base64String: String {
-        var bytes = [Byte](self)
-        let data = NSData(bytes: &bytes, count: bytes.count)
+        let bytes = [Byte](self)
+        let data = NSData(bytes: bytes, count: bytes.count)
         // TODO: Add Link
         // This part from Crypto Essentials
         #if os(Linux)
