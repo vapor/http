1b4fd3192d969f826dcc024699dd778de9f36b03
Logan Wright <logan.william.wright@gmail.com>
2016-07-06 03:02:50 -0400

    data nsdata confusion

--- Sources/ToolBox/Byte/Bytes+Base64.swift
+++ Sources/ToolBox/Byte/Bytes+Base64.swift
@@ -1,23 +1,22 @@
 import Foundation
 
-/*
- // TODO: Temporary while foundation naming differs on linux
- */
-
-#if !os(Linux)
-    typealias NSData = Foundation.Data
-#endif
-
 extension Sequence where Iterator.Element == Byte {
     public var base64String: String {
         let bytes = [Byte](self)
-        let data = NSData(bytes: bytes, count: bytes.count)
-        // TODO: Add Link
-        // This part from Crypto Essentials
+        let data = NSData(bytes: bytes)
         #if os(Linux)
             return data.base64EncodedString([])
         #else
             return data.base64EncodedString(options: [])
         #endif
+    }
+}
+
+
+extension NSData {
+    // TODO: Add Link
+    // This part from Crypto Essentials
+    convenience init(bytes: [UInt8]) {
+        self.init(bytes: bytes, length: bytes.count)
     }
 }
