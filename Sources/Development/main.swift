ea16728518e41c4dffda0299b5e23b27d369dffa
Logan Wright <logan.william.wright@gmail.com>
2016-07-06 02:02:39 -0400

    cleaning up main examples a bit

--- Sources/Development/main.swift
+++ Sources/Development/main.swift
@@ -1,21 +1,23 @@
 import Engine
 import WebSockets
 
-
-let response = try HTTPClient<TCPClientStream>.get("https://api.spotify.com/v1/search?q=beyonce&type=artist")
-print(response.body.bytes?.string)
-
-
-try WebSocket.connect(to: "ws://vapor-dashboard.herokuapp.com/updates") { ws in
-    print("connected")
-
-    ws.onText = { ws, text in
-        print("[ws] - \(text)")
-    }
-
-    ws.onClose = { _ in
-        print("CLOSED")
-    }
+func client() throws {
+    let response = try HTTPClient<TCPClientStream>.get("https://api.spotify.com/v1/search?q=beyonce&type=artist")
+    print(response.body.bytes?.string)
 }
 
-print("done")
+func server() throws {
+    final class Responder: HTTPResponder {
+        func respond(to request: Request) throws -> Response {
+            let body = "Hello World".makeBody()
+            return Response(body: body)
+        }
+    }
+
+    let server = try HTTPServer<TCPServerStream, HTTPParser<HTTPRequest>, HTTPSerializer<HTTPResponse>>()
+    try server.start(responder: Responder(), errors: { _ in })
+}
+
+// Call
+
+try server()
