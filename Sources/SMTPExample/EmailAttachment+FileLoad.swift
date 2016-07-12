957e1574facf3f025d610a4d4e28416b721c0e0d
Logan Wright <logan.william.wright@gmail.com>
2016-07-11 23:33:58 -0400

    file load

--- Sources/SMTPExample/EmailAttachment+FileLoad.swift
+++ Sources/SMTPExample/EmailAttachment+FileLoad.swift
@@ -21,8 +21,7 @@ extension EmailAttachment {
             let suffix = filename.components(separatedBy: ".").last,
             let mediaType = mediaTypes[suffix]
             else { return nil }
-        let url = NSURL.fileURL(withPath: directory.finished(with: "/") + filename)
-        guard let data = NSData(contentsOf: url) else { return nil }
+        guard let data = NSData(contentsOfFile: directory.finished(with: "/") + filename) else { return nil }
         var bytes = [UInt8](repeating: 0, count: data.length)
         data.getBytes(&bytes, length: data.length)
         self.init(filename: filename, contentType: mediaType, body: bytes)
