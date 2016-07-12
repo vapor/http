e38a41f691b91ad340e4d0a75f43c3f190086c39
Logan Wright <logan.william.wright@gmail.com>
2016-07-11 23:11:29 -0400

    uuid is also different

--- Sources/SMTP/Utility/NSUUID+SMTPMessageID.swift
+++ Sources/SMTP/Utility/NSUUID+SMTPMessageID.swift
@@ -2,6 +2,10 @@ import Foundation
 
 extension NSUUID {
     static var smtpMessageId: String {
+        #if os(Linux)
+        return NSUUID().UUIDString.components(separatedBy: "-").joined(separator: "")
+        #else
         return NSUUID().uuidString.components(separatedBy: "-").joined(separator: "")
+        #endif
     }
 }
