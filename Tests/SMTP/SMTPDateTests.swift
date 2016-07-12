5a0c238534862a8c2e5426362745970ca9b31782
Logan Wright <logan.william.wright@gmail.com>
2016-07-12 04:13:19 -0400

    ns classes not showing on linux tests only. passing on build

--- Tests/SMTP/SMTPDateTests.swift
+++ Tests/SMTP/SMTPDateTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Foundation
 @testable import SMTP
 
 class SMTPDateTests: XCTestCase {
--- Tests/SMTP/SMTPUUIDTests.swift
+++ Tests/SMTP/SMTPUUIDTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Foundation
 @testable import SMTP
 
 class SMTPUUIDTests: XCTestCase {
