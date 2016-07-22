c22200471b1b0cc20711e30ad26d60d67ee63bc1
Logan Wright <logan.william.wright@gmail.com>
2016-07-22 11:59:33 -0400

    concrete versions

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@ import PackageDescription
 
 let dependencies: [Package.Dependency] = [
     //Websockets
-    .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 0),
+    .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 9),
 
     //Sockets, used by the built in HTTP server
     .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 9),
 
     //CoreComponents
-    .Package(url: "https://github.com/qutheory/core.git", majorVersion: 0, minor: 0)
+    .Package(url: "https://github.com/qutheory/core.git", majorVersion: 0, minor: 2)
 ]
 
 let package = Package(
