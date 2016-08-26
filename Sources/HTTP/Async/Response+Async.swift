95afa59b57a02c19a2e07c1c5c09debb9cb28560
Logan Wright <logan.william.wright@gmail.com>
2016-08-25 20:35:41 -0400

    unlock portal

--- Sources/HTTP/Async/Response+Async.swift
+++ Sources/HTTP/Async/Response+Async.swift
@@ -1,22 +1,16 @@
-/*
-    Temporarily not available on Linux until Foundation's 'Dispatch apis are available
-*/
+import Foundation
 
-#if !os(Linux)
-
-    import Foundation
-
-    extension Response {
-        /**
-            Sometimes, asynchronicity is required within Vapor's synchronous environment. 
-            Use this function to enter an async context in which the 'promise' object can
-            be passed to multiple threads and called when appropriate
-        */
-        public static func async(
-            timeout: Double = 999_999_999,
-            _ handler: @escaping (Portal<ResponseRepresentable>) throws -> Void
-        ) throws -> ResponseRepresentable {
-            return try Portal.open(timeout: timeout, handler)
-        }
+extension Response {
+    /**
+        Sometimes, asynchronicity is required within Vapor's synchronous environment. 
+        Use this function to enter an async context in which the 'promise' object can
+        be passed to multiple threads and called when appropriate
+    */
+    public static func async(
+        timeout: Double = 999_999_999,
+        _ handler: @escaping (Portal<ResponseRepresentable>) throws -> Void
+    ) throws -> ResponseRepresentable {
+        return try Portal.open(timeout: timeout, handler)
     }
-#endif
+}
+
