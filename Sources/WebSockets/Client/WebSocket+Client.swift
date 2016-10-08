381d50c3708616c250118111dd468183b44107bd
David Moeller <davidmoeller@Davids-MacBook-Pro.local>
2016-10-08 15:37:44 +0200

    Damn Typo

--- Sources/WebSockets/Client/WebSocket+Client.swift
+++ Sources/WebSockets/Client/WebSocket+Client.swift
@@ -52,7 +52,7 @@ extension WebSocket {
 
         // Don't need to check version in server response
         guard response.headers.connection?.lowercased() == "upgrade" else { throw FormatError.missingConnectionHeader }
-        guard response.headers.upgrade.?lowercased() == "websocket" else { throw FormatError.missingUpgradeHeader }
+        guard response.headers.upgrade?.lowercased() == "websocket" else { throw FormatError.missingUpgradeHeader }
         guard case .switchingProtocols = response.status else { throw FormatError.invalidOrUnsupportedStatus }
         guard let accept = response.headers.secWebSocketAccept else { throw FormatError.missingSecAcceptHeader }
         let expected = try WebSocket.exchange(requestKey: requestKey)
