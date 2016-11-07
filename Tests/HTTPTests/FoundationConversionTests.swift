105cf0645237ea2268dc1b300836c06efa7abd9f
Logan Wright <logan.william.wright@gmail.com>
2016-11-07 13:57:29 -0500

    url to uri test

--- Tests/HTTPTests/FoundationConversionTests.swift
+++ Tests/HTTPTests/FoundationConversionTests.swift
@@ -16,7 +16,24 @@ class HTTPConversionTests: XCTestCase {
         XCTAssertEqual(uri.query, url.query)
         XCTAssertEqual(uri.fragment, url.fragment)
 
-        XCTAssertEqual("\(uri)", expectation)
+        XCTAssertEqual(uri.description, expectation)
         XCTAssertEqual(url.absoluteString, expectation)
+    }
+
+    func testUrlToUriConversion() throws {
+        let expectation = "https://google.com:443/search?foo=bar#frag"
+        let url = URL(string: expectation)!
+        let uri = url.makeURI()
+
+        XCTAssertEqual(url.scheme, uri.scheme)
+        XCTAssertEqual(url.user, uri.userInfo?.username)
+        XCTAssertEqual(url.password, uri.userInfo?.info)
+        XCTAssertEqual(url.host, uri.host)
+        XCTAssertEqual(url.port, uri.port)
+        XCTAssertEqual(url.query, uri.query)
+        XCTAssertEqual(url.fragment, uri.fragment)
+
+        XCTAssertEqual(url.absoluteString, expectation)
+        XCTAssertEqual(uri.description, expectation)
     }
 }
