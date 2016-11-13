06351fca47389d3cf2e9b6c0f48c678ece6e8621
andreasley <and@mac-mp-and.local>
2016-11-13 14:34:29 +0100

    testServer now uses a random port; added warning

--- Tests/HTTPTests/HTTPMiddlewareTests.swift
+++ Tests/HTTPTests/HTTPMiddlewareTests.swift
@@ -44,7 +44,8 @@ class HTTPMiddlewareTests: XCTestCase {
 
         // create a basic server that returns
         // request headers
-        let server = try BasicServer(host: "0.0.0.0", port: 8080, securityLayer: .none, middleware: [foo])
+        let server = try BasicServer(host: "0.0.0.0", port: 0, securityLayer: .none, middleware: [foo])
+        let assignedPort = try server.server.stream.localAddress().port
         let responder = Request.Handler({ request in
             return request.headers.description.makeResponse()
         })
@@ -55,7 +56,7 @@ class HTTPMiddlewareTests: XCTestCase {
         }
 
         // create a basic client ot query the server
-        let client = try BasicClient(scheme: "http", host: "0.0.0.0", port: 8080, securityLayer: .none, middleware: [])
+        let client = try BasicClient(scheme: "http", host: "0.0.0.0", port: Int(assignedPort), securityLayer: .none, middleware: [])
         let response = try client.request(.get, path: "/foo")
 
         // test to make sure basic server saw the
@@ -66,6 +67,8 @@ class HTTPMiddlewareTests: XCTestCase {
         // test to make sure the middleware
         // added headers to the response
         XCTAssertEqual(response.headers["bar"], "baz")
+
+        // WARNING: `server` will keep running in the background since there is no way to stop it. Its socket will continue to exist and the associated port will be in use until the xctest process exits.
     }
 
     func testServerAsync() throws {
