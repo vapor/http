5c090199894f1d8ea8bd5302bd3d01bd5e8ca609
andreasley <and@mac-mp-and.local>
2016-11-13 12:50:33 +0100

    Restored public API
    Moved nonblocking code to separate functions so that the public API
    stays intact

--- Sources/HTTP/Models/Server/HTTP+Server.swift
+++ Sources/HTTP/Models/Server/HTTP+Server.swift
@@ -49,23 +49,80 @@ public final class Server<
         // add middleware
         let responder = middleware.chain(to: responder)
 
+        // no throwing inside of the loop
+        while true {
+            let stream: Stream
+
+            do {
+                stream = try server.accept()
+            } catch {
+                errors(.accept(error))
+                continue
+            }
+
+            queue.async {
+                do {
+                    try self.respond(stream: stream, responder: responder)
+                } catch {
+                    errors(.dispatch(error))
+                }
+
+            }
+        }
+    }
+
+    private func respond(stream: Stream, responder: Responder) throws {
+        let stream = StreamBuffer(stream)
+        try stream.setTimeout(30)
+
+        let parser = Parser(stream: stream)
+        let serializer = Serializer(stream: stream)
+
+        defer {
+            try? stream.close()
+        }
+
+        var keepAlive = false
+        repeat {
+            let request: Request
+            do {
+                request = try parser.parse()
+            } catch ParserError.streamEmpty {
+                // if stream is empty, it's time to
+                // close the connection
+                break
+            } catch {
+                throw error
+            }
+
+            keepAlive = request.keepAlive
+            let response = try responder.respond(to: request)
+            try serializer.serialize(response)
+            try response.onComplete?(stream)
+        } while keepAlive && !stream.closed
+    }
+
+    public func startAsync(responder: Responder, errors: @escaping ServerErrorHandler) throws {
+        // add middleware
+        let responder = middleware.chain(to: responder)
+        
         // await connection attempts on the server socket
         try server.startWatching(on: queue) { [weak self] in
             guard let welf = self else { return }
             do {
                 let stream = try welf.server.accept()
                 let bufferedStream = StreamBuffer(stream)
-                try welf.respond(stream: bufferedStream, responder: responder, errors: errors)
+                try welf.respondAsync(stream: bufferedStream, responder: responder, errors: errors)
             } catch {
                 errors(.accept(error))
             }
         }
     }
-
-    private func respond(stream: Stream, responder: Responder, errors: @escaping ServerErrorHandler) throws {
+    
+    private func respondAsync(stream: Stream, responder: Responder, errors: @escaping ServerErrorHandler) throws {
         let parser = Parser(stream: stream)
         let serializer = Serializer(stream: stream)
-
+        
         // await data on `stream`
         try stream.startWatching(on: queue) {
             // stream, parser and serializer are retained by the closure.
@@ -85,4 +142,5 @@ public final class Server<
             }
         }
     }
+
 }
--- Sources/HTTP/Server/Server.swift
+++ Sources/HTTP/Server/Server.swift
@@ -2,20 +2,35 @@ import Transport
 
 public protocol ServerProtocol: Program {
     func start(responder: Responder, errors: @escaping ServerErrorHandler) throws
+    func startAsync(responder: Responder, errors: @escaping ServerErrorHandler) throws
 }
 
 extension ServerProtocol {
+
     public static func start(
         host: String? = nil,
         port: Int? = nil,
         securityLayer: SecurityLayer = .none,
         responder: Responder,
         errors: @escaping ServerErrorHandler
-    ) throws -> Self {
+        ) throws {
         let server = try make(host: host, port: port, securityLayer: securityLayer)
         let responder = responder
         let errors = errors
         try server.start(responder: responder, errors: errors)
+    }
+
+    public static func startAsync(
+        host: String? = nil,
+        port: Int? = nil,
+        securityLayer: SecurityLayer = .none,
+        responder: Responder,
+        errors: @escaping ServerErrorHandler
+        ) throws -> Self {
+        let server = try make(host: host, port: port, securityLayer: securityLayer)
+        let responder = responder
+        let errors = errors
+        try server.startAsync(responder: responder, errors: errors)
         return server
     }
 }
