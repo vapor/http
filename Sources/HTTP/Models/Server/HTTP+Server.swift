a99e79619e24f22c99c1aab72acbb551fbe0c4ba
andreasley <and@mac-mp-and.local>
2016-12-07 08:05:35 +0100

    Updated comments to reflect the need for error logging

--- Sources/HTTP/Models/Server/HTTP+Server.swift
+++ Sources/HTTP/Models/Server/HTTP+Server.swift
@@ -148,10 +148,10 @@ public final class Server<
                 try? stream.close()
             } catch let error where error is StreamError {
                 // if there's a problem with the stream, there's no point in keeping it open.
-                // reporting the error is not necessary here; there are various legitimate
-                // reasons for socket connections to be broken.
                 self?.streams.remove(stream)
                 try? stream.close()
+                // reporting the error is not strictly necessary here (there are  legitimate
+                // reasons for socket connections to be broken), but helpful for debugging.
                 errors(.respond(error))
             } catch {
                 errors(.respond(error))
