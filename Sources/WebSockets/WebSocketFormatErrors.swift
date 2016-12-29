cbf5241b7198b2fd9ebcd0cdb8364c16e15624c4
Charlie Woloszynski <charles.woloszynski@handheldmedia.us>
2016-12-29 15:05:03 -0500

    Add equatable to support the new enumeration in FormatError

--- Sources/WebSockets/WebSocketFormatErrors.swift
+++ Sources/WebSockets/WebSocketFormatErrors.swift
@@ -12,3 +12,29 @@ extension WebSocket {
         case invalidOrUnsupportedStatus(Status)
     }
 }
+
+extension WebSocket.FormatError: Equatable {
+    public static func ==(lhs: WebSocket.FormatError, rhs: WebSocket.FormatError) -> Bool {
+        switch (lhs, rhs) {
+        case (.missingSecKeyHeader, .missingSecKeyHeader):
+            return true
+        case (.missingSecAcceptHeader, .missingSecAcceptHeader):
+            return true
+        case (.invalidSecAcceptHeader, .invalidSecAcceptHeader):
+            return true
+        case (.missingUpgradeHeader, .missingUpgradeHeader):
+            return true
+        case (.missingConnectionHeader, .missingConnectionHeader):
+            return true
+        case (.invalidURI, .invalidURI):
+            return true
+        case (.invalidOrUnsupportedVersion, .invalidOrUnsupportedVersion):
+            return true
+        case (.invalidOrUnsupportedStatus(let lhsStatus), .invalidOrUnsupportedStatus(let rhsStatus)):
+            return lhsStatus == rhsStatus
+        default:
+            return false
+        }
+    }
+}
+
