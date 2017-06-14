fe40e0a99058ad0e4a6029b994a5917e5d4898a7
Logan Wright <logan.william.wright@gmail.com>
2017-06-14 14:41:01 -0400

    add more detailed redirect

--- Sources/HTTP/Response/Response+Redirect.swift
+++ Sources/HTTP/Response/Response+Redirect.swift
@@ -1,8 +1,58 @@
-//
-//  Response+Redirect.swift
-//  HTTP
-//
-//  Created by Logan Wright on 6/14/17.
-//
+public enum RedirectType {
+    /// A permanent redirect
+    case permanent // 301 permanent
+    /// Forces the redirect to come with a GET, regardless of req method
+    case normal // 303 see other
+    /// Maintains original request method, ie: PUT will call PUT on redirect
+    case temporary // 307 temporary
+}
 
-import Foundation
+extension RedirectType {
+    fileprivate var status: Status {
+        switch self {
+        case .permanent:
+            return .permanentRedirect
+        case .normal:
+            return .seeOther
+        case .temporary:
+            return .temporaryRedirect
+        }
+    }
+}
+extension Response {
+    /// Creates a redirect response.
+    ///
+    /// Set permanently to 'true' to allow caching to automatically
+    /// redirect from browsers.
+    /// Defaulting to non-permanent to prevent unexpected caching.
+    public convenience init(
+        headers: [HeaderKey: String] = [:],
+        redirect location: String,
+        permanently: Bool = false
+    ) {
+        self.init(
+            headers: headers,
+            redirect: location,
+            type: permanently ? .permanent : .normal
+        )
+    }
+}
+
+extension Response {
+    /// Creates a redirect response.
+    ///
+    /// Set permanently to 'true' to allow caching to automatically
+    /// redirect from browsers.
+    /// Defaulting to non-permanent to prevent unexpected caching.
+    public convenience init(
+        headers: [HeaderKey: String] = [:],
+        redirect location: String,
+        type: RedirectType = .normal
+    ) {
+        var headers = headers
+        headers["Location"] = location
+        self.init(status: type.status, headers: headers)
+    }
+}
+
+
