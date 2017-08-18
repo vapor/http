a1ee304c65d187309fccd2997be9bd168ff89c46
Joannis Orlandos <joannis@orlandos.nl>
2017-08-18 08:19:40 +0200

    More comments for the socket layer

--- Sources/Sockets/TCPSocket.swift
+++ Sources/Sockets/TCPSocket.swift
@@ -29,12 +29,17 @@ public class TCPSocket {
     /// For clients, the hostname that is being connected to
     var socketAddress = UnsafeMutablePointer<sockaddr_storage>.allocate(capacity: 1)
     
-    public init(hostname: String, port: UInt16, dispatchQueue queue: DispatchQueue? = nil) throws {
+    public init(hostname: String, port: UInt16, client: Bool, dispatchQueue queue: DispatchQueue? = nil) throws {
         var addressCriteria = addrinfo.init()
-        // IPv4 or IPv6
-        addressCriteria.ai_family = Int32(AF_INET)
-        addressCriteria.ai_flags = AI_PASSIVE
         
+        // Support both IPv4 and IPv6
+        addressCriteria.ai_family = Int32(AF_INET)
+        
+        if !client {
+            addressCriteria.ai_flags = AI_PASSIVE
+        }
+        
+        // Specify that this is a TCP Stream
         #if os(Linux)
             addressCriteria.ai_socktype = Int32(SOCK_STREAM.rawValue)
             addressCriteria.ai_protocol = Int32(IPPROTO_TCP)
@@ -43,17 +48,18 @@ public class TCPSocket {
             addressCriteria.ai_protocol = IPPROTO_TCP
         #endif
         
+        // Look ip the sockeaddr for the hostname
         var addrInfo: UnsafeMutablePointer<addrinfo>?
         
         guard getaddrinfo(hostname, port.description, &addressCriteria, &addrInfo) > -1 else {
-            throw TCPError.bindFailed
+            throw TCPError.bindFailure
         }
         
-        guard let info = addrInfo else { throw TCPError.bindFailed }
+        guard let info = addrInfo else { throw TCPError.bindFailure }
         
         defer { freeaddrinfo(info) }
         
-        guard let addr = info.pointee.ai_addr else { throw TCPError.bindFailed }
+        guard let addr = info.pointee.ai_addr else { throw TCPError.bindFailure }
         
         socketAddress.initialize(to: sockaddr_storage())
         
@@ -61,23 +67,46 @@ public class TCPSocket {
         let specPtr = UnsafeMutablePointer<sockaddr_in>(OpaquePointer(socketAddress))
         specPtr.assign(from: _addr, count: 1)
         
+        // Open the socket, get the descriptor
         self.descriptor = socket(addressCriteria.ai_family, addressCriteria.ai_socktype, addressCriteria.ai_protocol)
         
         guard descriptor > -1 else {
-            throw TCPError.bindFailed
+            throw TCPError.bindFailure
         }
         
+        // Set the socket to async/non blocking I/O
         guard fcntl(self.descriptor, F_SETFL, O_NONBLOCK) > -1 else {
-            throw TCPError.bindFailed
+            throw TCPError.bindFailure
         }
         
         var yes = 1
         
         guard setsockopt(self.descriptor, SOL_SOCKET, SO_REUSEADDR, &yes, socklen_t(MemoryLayout<Int>.size)) > -1 else {
-            throw TCPError.bindFailed
+            throw TCPError.bindFailure
         }
         
+        // Set up the read source so that reading happens asynchronously using DispatchSources
         self.readSource = DispatchSource.makeReadSource(fileDescriptor: descriptor, queue: queue ?? TCPSocket.queue)
+        
+        self.readSource.setCancelHandler(handler: self.cleanup)
+    }
+    
+    /// Called when closing the socket
+    ///
+    /// In charge of cleaning everything, from the socket's file descriptor to the SSL layer and anything extra
+    ///
+    /// Do **not** call this manually, or your application will very likely crash.
+    open func cleanup() {
+        #if os(Linux)
+            Glibc.close(self.descriptor)
+        #else
+            Darwin.close(self.descriptor)
+        #endif
+    }
+    
+    /// Closes the socket.
+    open func close() {
+        self.readSource.cancel()
     }
     
     /// The file descriptor related to this socket
--- Sources/Sockets/TCPWriteSocket.swift
+++ Sources/Sockets/TCPWriteSocket.swift
@@ -8,5 +8,29 @@ import Dispatch
 #endif
 
 extension TCPSocket {
-    
+    public func write(contentsAt pointer: UnsafePointer<UInt8>, withLengthOf length: Int) throws -> Int {
+        #if os(Linux)
+            let sent = Glibc.send(self.descriptor, pointer, length, 0)
+        #else
+            let sent = Darwin.send(self.descriptor, pointer, length, 0)
+        #endif
+        
+        guard sent != -1 else {
+            switch errno {
+            case EINTR:
+                // try again
+                return try self.write(contentsAt: pointer, withLengthOf: length)
+            case ECONNRESET:
+                // closed by peer, need to close this side.
+                // Since this is not an error, no need to throw unless the close
+                // itself throws an error.
+                self.close()
+                return 0
+            default:
+                throw TCPError.sendFailure
+            }
+        }
+        
+        return sent
+    }
 }
