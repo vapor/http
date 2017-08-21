683e750f9edc2a6c43adf41319e014b801f0a67a
Joannis Orlandos <joannis@orlandos.nl>
2017-08-21 19:16:12 +0200

    split off middlewares

---
+++ Sources/HTTP/Responder/Middleware.swift
@@ -0,0 +1,16 @@
+import Core
+
+public protocol RequestMiddleware : Core.Stream {
+    associatedtype Input = Request
+    associatedtype Output = Request
+}
+
+public protocol ResponseMiddleware : Core.Stream {
+    associatedtype Input = Response
+    associatedtype Output = Response
+}
+
+public protocol Middleware : Core.Stream {
+    associatedtype Input = Request
+    associatedtype Output = Response
+}
--- Sources/WebSockets/WebSocket.swift
+++ Sources/WebSockets/WebSocket.swift
@@ -50,16 +50,6 @@ public class WebSocket {
     }
 }
 
-public protocol RequestMiddleware : Core.Stream {
-    associatedtype Input = Request
-    associatedtype Output = Request
-}
-
-public protocol ResponseMiddleware : Core.Stream {
-    associatedtype Input = Response
-    associatedtype Output = Response
-}
-
 public class WebSocketMiddleware : RequestMiddleware {
     public typealias Input = Request
     public typealias Output = Request
