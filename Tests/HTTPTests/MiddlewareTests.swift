45893307715a08f02139115d37d793c48550d24e
Tanner Nelson <me@tanner.xyz>
2017-08-21 20:16:33 -0400

    cleanup tests

--- Tests/HTTPTests/MiddlewareTests.swift
+++ Tests/HTTPTests/MiddlewareTests.swift
@@ -1,4 +1,5 @@
 import Core
+import Dispatch
 import HTTP
 import XCTest
 
@@ -6,18 +7,27 @@ class MiddlewareTests : XCTestCase {
     func testMiddleware() throws {
         let server = EmitterStream<Request>()
 
-        let app = TestApp()
+        let group = DispatchGroup()
+        group.enter()
+        let app = TestApp { req in
+            XCTAssertEqual(req.headers["foo"], "bar")
+            group.leave()
+        }
+
         let middlewares = [TestMiddleware(), TestMiddleware()]
         let responder = middlewares.makeResponder(chainedto: app)
 
         let queue = DispatchQueue(label: "codes.vapor.http.test.middleware")
 
-        server.stream(to: responder.makeStream(on: queue)).drain { response in
-            print(response.headers)
+        group.enter()
+        server.stream(to: responder.makeStream(on: queue)).drain { res in
+            XCTAssertEqual(res.headers["baz"], "bar")
+            group.leave()
         }
 
         let req = Request()
         server.emit(req)
+        group.wait()
     }
 
     static let allTests = [
@@ -25,13 +35,22 @@ class MiddlewareTests : XCTestCase {
     ]
 }
 
+/// Test application that passes all incoming
+/// requests through a closure for testing
 final class TestApp: Responder {
+    let closure: (Request) -> ()
+
+    init(closure: @escaping (Request) -> ()) {
+        self.closure = closure
+    }
+
     func respond(to req: Request) throws -> Future<Response> {
-        print(req.headers)
+        closure(req)
         return Future { Response() }
     }
 }
 
+/// Test middleware that sets req and res headers.
 final class TestMiddleware: Middleware {
     func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
         request.headers["foo"] = "bar"
