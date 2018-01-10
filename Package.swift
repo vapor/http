8de2136fdbe754bf15b45e25e035e28bee00eef7
Joannis Orlandos <joannis@orlandos.nl>
2018-01-10 19:01:49 +0100

    update async branch

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .branch("async")),
+        .package(url: "https://github.com/vapor/async.git", .branch("beta")),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks.
         .package(url: "https://github.com/vapor/core.git", .branch("beta")),
