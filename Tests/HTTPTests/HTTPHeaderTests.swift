f4cf3d5819437445531bcc02da137618e0e444a2
Tanner Nelson <me@tanner.xyz>
2018-01-29 17:41:46 -0500

    add google headers test

--- Tests/HTTPTests/HTTPHeaderTests.swift
+++ Tests/HTTPTests/HTTPHeaderTests.swift
@@ -1,6 +1,6 @@
 import Async
 import Bits
-import HTTP
+@testable import HTTP
 import Foundation
 import TCP
 import XCTest
@@ -40,9 +40,45 @@ class HTTPHeaderTests: XCTestCase {
         XCTAssertEqual(headers[.contentType], "text/plain")
     }
 
+    func testHeadersGoogle() throws {
+        let text = """
+        Content-Type: text/html; charset=UTF-8\r
+        Referrer-Policy: no-referrer\r
+        Content-Length: 1564\r
+        Date: Mon, 29 Jan 2018 22:29:53 GMT\r
+        Alt-Svc: hq=":443"; ma=2592000; quic=51303431; quic=51303339; quic=51303338; quic=51303337; quic=51303335,quic=":443"; ma=2592000; v="41,39,38,37,35"\r
+        """
+        let storage = HTTPHeaderStorage(bytes: Bytes(text.utf8), indexes: [
+            HTTPHeaderIndex(nameStartIndex: 0, nameEndIndex: 12, valueStartIndex: 14, valueEndIndex: 38),
+            HTTPHeaderIndex(nameStartIndex: 40, nameEndIndex: 55, valueStartIndex: 57, valueEndIndex: 68),
+            HTTPHeaderIndex(nameStartIndex: 70, nameEndIndex: 84, valueStartIndex: 86, valueEndIndex: 90),
+            HTTPHeaderIndex(nameStartIndex: 92, nameEndIndex: 96, valueStartIndex: 98, valueEndIndex: 127),
+            HTTPHeaderIndex(nameStartIndex: 129, nameEndIndex: 136, valueStartIndex: 138, valueEndIndex: 278),
+        ])
+        var headers = HTTPHeaders(storage: storage)
+        XCTAssertEqual(headers.description, text)
+        XCTAssertEqual(headers[.contentType], "text/html; charset=UTF-8")
+        XCTAssertEqual(headers["Referrer-Policy"], "no-referrer")
+        XCTAssertEqual(headers[.contentLength], "1564")
+        XCTAssertEqual(headers[.date], "Mon, 29 Jan 2018 22:29:53 GMT")
+        XCTAssertEqual(headers["Alt-Svc"], """
+        hq=":443"; ma=2592000; quic=51303431; quic=51303339; quic=51303338; quic=51303337; quic=51303335,quic=":443"; ma=2592000; v="41,39,38,37,35"
+        """)
+
+        headers[.contentLength] = "1564"
+        XCTAssertEqual(headers[.contentType], "text/html; charset=UTF-8")
+        XCTAssertEqual(headers["Referrer-Policy"], "no-referrer")
+        XCTAssertEqual(headers[.contentLength], "1564")
+        XCTAssertEqual(headers[.date], "Mon, 29 Jan 2018 22:29:53 GMT")
+        XCTAssertEqual(headers["Alt-Svc"], """
+        hq=":443"; ma=2592000; quic=51303431; quic=51303339; quic=51303338; quic=51303337; quic=51303335,quic=":443"; ma=2592000; v="41,39,38,37,35"
+        """)
+
+    }
 
     static let allTests = [
         ("testHeaders", testHeaders),
         ("testHeaderDisplacement", testHeaderDisplacement),
+        ("testHeadersGoogle", testHeadersGoogle),
     ]
 }
