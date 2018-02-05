9e4104471f2ca943c2d67b25f974fe0a47accfbb
Jonas Schwartz <jonas@qutheory.io>
2018-02-04 23:15:47 -0500

    Removing static vars in TCP

--- Sources/HTTP/Client/HTTPClient+TCP.swift
+++ Sources/HTTP/Client/HTTPClient+TCP.swift
@@ -6,7 +6,7 @@ extension HTTPClient {
     public static func tcp(hostname: String, port: UInt16, on worker: Worker) throws -> HTTPClient {
         let tcpSocket = try TCPSocket(isNonBlocking: true)
         let tcpClient = try TCPClient(socket: tcpSocket)
-        try tcpClient.connect(hostname: "httpbin.org", port: 80)
+        try tcpClient.connect(hostname: hostname, port: port)
         return HTTPClient(stream: tcpSocket.stream(on: worker), on: worker)
     }
 }
