6a856c03bd3ccda7383956b3214adbc0bb97262f
Tanner Nelson <me@tanner.xyz>
2018-03-01 20:03:13 -0500

    add build-essentials

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -5,20 +5,13 @@ import XCTest
 @testable import MultipartTests
 @testable import FormURLEncodedTests
 XCTMain([
-	// MARK: HTTP
     testCase(HTTPClientTests.allTests),
-    testCase(HTTPServerTests.allTests),
-    testCase(HTTPParserTests.allTests),
-    testCase(HTTPSerializerTests.allTests),
-    testCase(UtilityTests.allTests),
 
     testCase(FormURLEncodedCodableTests.allTests),
     testCase(FormURLEncodedParserTests.allTests),
     testCase(FormURLEncodedSerializerTests.allTests),
 
-    // MARK: TCP
     testCase(MultipartTests.allTests),
-    testCase(SubProtocolMatcherTests.allTests),
 ])
 
 #endif
