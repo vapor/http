5010a61e3d84fc6116a0e035c55c97e093de28de
tanner0101 <me@tanner.xyz>
2018-03-08 17:27:53 -0500

    make example code more concise

--- Sources/Performance/main.swift
+++ Sources/Performance/main.swift
@@ -7,36 +7,8 @@ let port: Int = 8123
 
 struct EchoResponder: HTTPResponder {
     func respond(to req: HTTPRequest, on worker: Worker) -> Future<HTTPResponse> {
-        let body: HTTPBody
-
-        if req.url.path == "/chunked" {
-            let stream = HTTPChunkedStream(on: worker)
-            body = HTTPBody(chunked: stream)
-
-            var buffer = ByteBufferAllocator().buffer(capacity: 256)
-            buffer.write(string: "hello")
-            stream.write(.chunk(buffer)).flatMap(to: Void.self) {
-                buffer.clear()
-                buffer.write(string: "world")
-                return stream.write(.chunk(buffer))
-            }.flatMap(to: Void.self) {
-                buffer.clear()
-                buffer.write(string: "!")
-                return stream.write(.chunk(buffer))
-            }.flatMap(to: Void.self) {
-                return stream.write(.end)
-            }.do {
-                print("done")
-            }.catch { error in
-                print("error: \(error)")
-            }
-        } else {
-            body = HTTPBody(string: "Hello, world!")
-        }
-
-        return Future.map(on: worker) {
-            return HTTPResponse(body: body)
-        }
+        let res = HTTPResponse(body: HTTPBody(string: "Hello, world!"))
+        return Future.map(on: worker) { res }
     }
 }
 
