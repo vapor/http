7679bc3a354593743830fc593085a84e13ed44bb
grundoon <grundoon@gmail.com>
2019-02-18 09:32:38 -0800

    test RFC1123 seconds boundary flip

--- Tests/HTTPKitTests/Utilities.swift
+++ Tests/HTTPKitTests/Utilities.swift
@@ -11,4 +11,16 @@ class HTTPKitTestCase: XCTestCase {
     override func tearDown() {
         try! self.eventLoopGroup.syncShutdownGracefully()
     }
+
+    func testRFC1123Flip() throws {
+        let nowStamp = Date().rfc1123
+        let boundary = 1.0 - Date().timeIntervalSince1970.truncatingRemainder(dividingBy: 1)
+        Thread.sleep(forTimeInterval: boundary - 0.01)
+        let beforeStamp = Date().rfc1123
+        Thread.sleep(forTimeInterval: 0.02)
+        let afterStamp = Date().rfc1123
+        
+        XCTAssertEqual(nowStamp, beforeStamp)
+        XCTAssertNotEqual(beforeStamp, afterStamp)
+    }
 }
