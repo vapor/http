8c593132173b13e8b41ff1b83d5fb8d05d5dbd5d
grundoon <grundoon@gmail.com>
2019-02-19 08:25:02 -0800

    fix spacing (why github, why?)

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -29,7 +29,7 @@ extension HTTPKitTests.HTTPTests {
 		("testUncleanShutdown", testUncleanShutdown),
 		("testClientProxyPlaintext", testClientProxyPlaintext),
 		("testClientProxyTLS", testClientProxyTLS),
-                ("testRFC1123Flip", testRFC1123Flip),
+		("testRFC1123Flip", testRFC1123Flip),
 	]
 }
 
